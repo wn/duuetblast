dc73580c181804823279530085fffdc833f50fcc
wn <weineng.a@gmail.com>
2019-03-03 23:28:23 +0800

    Loop bg infinite

--- Memeblast/Supporting Files/AppDelegate.swift
+++ Memeblast/Supporting Files/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
 
-        Settings.playSoundWith(Constants.backgroundMusic)
+        Settings.playSoundWith(Constants.backgroundMusic, loop: -1, vol: 0.3)
         return true
     }
 
